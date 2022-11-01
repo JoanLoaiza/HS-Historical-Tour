c20c269bb9b47c0e43fc35f818600b433c6ad6be
Joan <joan.loaiza@ucp.edu.co>
2022-11-01 11:11:01 -0500

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,3 @@
+<div class="copyrights">
+	 <p>© 2022 HS. Todos los derechos reservados |  <a href="#">HS</a> </p>
+</div>	
---
+++ check_availability.php
@@ -0,0 +1,27 @@
+<?php 
+require_once("includes/config.php");
+// code admin email availablity
+if(!empty($_POST["emailid"])) {
+	$email= $_POST["emailid"];
+	if (filter_var($email, FILTER_VALIDATE_EMAIL)===false) {
+
+		echo "error : No has escrito un correo electrónico válido";
+	}
+	else {
+		$sql ="SELECT EmailId FROM tblusers WHERE EmailId=:email";
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':email', $email, PDO::PARAM_STR);
+$query-> execute();
+$results = $query -> fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query -> rowCount() > 0)
+{
+echo "<span style='color:red'> el email existe .</span>";
+ echo "<script>$('#submit').prop('disabled',true);</script>";
+} else{
+	
+	echo "<span style='color:green'> El email disponible para registro .</span>";
+ echo "<script>$('#submit').prop('disabled',false);</script>";
+}
+}
+}
